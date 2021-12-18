180a2880272bab377fded5779e6c9a8fea861e18
madelinerogers0 <92342179+madelinerogers0@users.noreply.github.com>
2021-12-18 12:49:32 -0600

    Add files via upload

---
+++ DogonPotVariation/DogonPotVariation/Pot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogonPotVariation
+{
+    class Pot
+    {
+        private int diameter = 9;
+        private int minimumDiameter = 1;
+        private string measurementSystem = "inches";
+        private int step = 1;
+       
+        
+       
+        public Pot()
+        {
+            Console.WriteLine("Steps to draw a Dogon-like pot:");
+            DrawPot();
+        }
+
+        private void DrawPot()
+        {
+           
+
+            if (diameter <= minimumDiameter)
+            {
+                Console.WriteLine($"{ step}. Draw the lid");
+            }
+            else
+            {
+                Console.WriteLine($"{step}. Draw a circle that is {diameter} {measurementSystem} in size");
+                step++;
+                diameter = 2 * (diameter/3);
+            
+                DrawPot(); 
+            }
+        } 
+    }
+}
+    
---
+++ DogonPotVariation/DogonPotVariation/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * /Madeline Rogers with help from Elias Olson
+ * Recursion Assignment
+ * 9/29/2021
+ * Based on Dogon Pot pseudo code from the book
+ * African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogonPotVariation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
