af017b52e62da57f873631140bd00a29afc869cf
madelinerogers0 <92342179+madelinerogers0@users.noreply.github.com>
2021-12-18 12:48:18 -0600

    Add files via upload

---
+++ Ten Bears/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ten_Bears
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bearsNeeded = 6;
+            int bearTotal = 0;
+            Console.WriteLine("A while loop. Total bears: " + bearTotal);
+            while (bearTotal < bearsNeeded)
+            {
+                MakeBear();
+                bearTotal++;
+            }
+            Console.WriteLine("A do while loop. Total bears: " + bearTotal);
+            do
+            {
+                MakeBear();
+                bearTotal++;
+            } while (bearTotal < bearsNeeded);
+
+            Console.WriteLine("A for loop.");
+            int total = 2;
+            for (int counter = 0; counter < total; counter++)
+            {
+                MakeBear();
+            }
+
+            Console.ReadLine();
+        }
+        static void MakeBear()
+        {
+            string bear = @"
+                    {'' - '' }
+                    ( .    .  )
+                ,--  `   Y   '--.
+                ````:         ; ''
+                   /    ___   \
+                 (    )'   `(   )
+                ";
+            Console.WriteLine(bear);
+        }
+    }
+}
